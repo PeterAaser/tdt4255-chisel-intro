74fe9b7065bb861d141cfff528f46f589386e525
peteraa <peteraaser@gmail.com>
2019-05-03 19:14:13 +0200

    split into two files

--- src/main/scala/Vector.scala
+++ src/main/scala/Vector.scala
@@ -7,21 +7,29 @@ class Vector(val elements: Int) extends Module {
 
   val io = IO(
     new Bundle {
-      val idx        = Input(UInt(32.W))
-      val dataIn     = Input(UInt(32.W))
-      val readEnable = Input(Bool())
+      val idx         = Input(UInt(32.W))
+      val dataIn      = Input(UInt(32.W))
+      val writeEnable = Input(Bool())
 
-      val dataOut    = Output(UInt(32.W))
+      val dataOut     = Output(UInt(32.W))
     }
   )
 
-  /**
-    * Your code here
-    */
-
   // Creates a vector of zero-initialized registers
-  val contents = RegInit(VecInit(List.fill(elements)(0.U(32.W))))
+  val internalVector = RegInit(VecInit(List.fill(elements)(0.U(32.W))))
 
-  // placeholder
+
+  when(writeEnable){
+    // TODO:
+    // When writeEnable is true the content of internalVector at the index specified
+    // by idx should be set to the value of io.dataIn
+  }
+  // In this case we don't want an otherwise block, in writeEnable is low we don't change
+  // anything
+
+
+  // TODO:
+  // io.dataOut should be driven by the contents of internalVector at the index specified
+  // by idx
   io.dataOut := 0.U
 }
