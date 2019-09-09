0cdbf4a5755eeaa6d05b7eed495fefcdb71eab17
peteraa <peteraaser@gmail.com>
2019-09-09 15:50:36 +0200

    Add delivery script

---
+++ deliver.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -p "Enter your student username (the one you use on badboard): "  username
+echo "Cleaning your project"
+
+# ./sbt.sh clean
+
+echo "Creating archive"
+mkdir wrap
+mkdir wrap/project
+cp -r src ./wrap/
+cp build.sbt ./wrap
+cp project/Dependencies.scala ./wrap/project/Dependencies.scala
+cp project/build.properties ./wrap/project/build.properties
+cp sbt.sh ./wrap
+(cd ./wrap/; tar czfv $username.gz .)
+mv ./wrap/$username.gz .
+
+rm -rf ./wrap
+
+echo "Unwrapping and testing your wrapped package"
+mkdir wrapTest
+tar -C ./wrapTest -xvf $username.gz
+./wrapTest/sbt.sh test
+rm -rf ./wrapTest
+
+echo "If the test output looked good then you're good to go!"
